eb27ebeae1c2b70187bef469bb0c26f6a46d81cd
Mahesh Kakde <maheshkakde100@gmail.com>
2024-04-13 14:51:13 +0530

    findal update

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -1,23 +1,14 @@
-import React from "react";
-//import {TodoItem} from "./MyComponents/TodoItem";
-
-// export default function TodoItem({todo})
-// {
-//     return(
-//         <div>
-//           <h4> {todo.title}</h4>
-//           <p>{todo.desc}</p>
-//         </div>
-//     )
-// }
+import React from 'react'
 
 export const TodoItem = ({todo, onDelete}) => {
-    return(
+    return (
+        <>
         <div>
-            <h4>{todo.title}</h4>
-            <p>{todo.desc}</p>
-            <button  className="btn btn-sm btn-danger" onClick ={()=> {onDelete(todo)}}
-            >Delete</button>
+           <h4>{todo.title}</h4>
+           <p>{todo.desc}</p>
+           <button className="btn btn-sm btn-danger" onClick={()=>{onDelete(todo)}}>Delete</button> 
         </div>
+        <hr/> 
+        </>
     )
 }
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,57 +1,5 @@
-import React from "react";
-//import TodoItem from "./TodoItem";
-import {TodoItem} from "../MyComponents/TodoItem";
-
-// export default function Todos(props)
-// {
-//     return(
-//         <div className="container">
-//           <h3>Todos List</h3>
-//           {/* {props.todos} */}
-//           <TodoItem todo ={props.todo[0]}/>
-
-//         </div>
-//     )
-// }
-
-// export const Todos = (props) => {
-//     return(
-//         <div className="container">
-//             <h3 className ="my-3"> Todos List :- </h3>
-//             {props.todos?.length===0? "No Todos to display": //<p className="text-center"></p></p>:
-//           props.todos.map((todo)=>{
-//             return(  
-//                 <>
-//                  <TodoItem todo = {todo} key={todo.sno} onDelete = {props.onDelete}/> <hr/>
-//                 </>
-//           )
-//         })
-//           }  
-            
-//         </div>
-//     )
-// }
-
-// export const Todos = (props) => {
-//     let myStyle = {
-//         minHeight: "70vh",
-//         margin: "40px auto"
-//     }
-//     return (
-//         <div className="container" style={myStyle}>
-//             <h3 className="my-3">Todos List</h3>
-//             {props.todos.length === 0 ? "No Todos to display" :
-//                 props.todos.map((todo) => {
-//                     return
-//                     <>
-//                         <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete} /> <hr />
-//                     </>
-
-//                 })
-//             }
-//         </div>
-//     )
-// }
+import React from 'react'
+import {TodoItem} from './TodoItem';
 
 export const Todos = (props) => {
     const todos = props.todos || []; // Ensure todos is an array, default to empty array if undefined
@@ -71,4 +19,3 @@ export const Todos = (props) => {
         </div>
     );
 }
-
